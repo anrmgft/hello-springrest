da4cfd7a2d52585e840ac6648bc69fe9e9d445b2
anrmgft <anrm@gft.com>
2022-06-10 14:09:28 +0200

    3 test

--- rest-service/src/test/java/com/example/restservice/GreetingControllerTest.java
+++ rest-service/src/test/java/com/example/restservice/GreetingControllerTest.java
@@ -30,4 +30,19 @@ class GreetingControllerTest {
                 .andExpect(jsonPath("$.content").value("Hola k ase"));
 
     }
+
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    }
 }
